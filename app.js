59a5069dd61a95c66e4e59f8c52ba2d62d4518a7
Denys Gaspar <53532689+dgaspar1@users.noreply.github.com>
2023-11-20 17:37:52 -0300

    feat: including css

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import Tweets from './models/Tweets.js'
 import env from './configs/env.js'
 import { fileURLToPath } from 'url'
 import path from 'path'
-import Sequelize from 'sequelize';
+import Sequelize from 'sequelize'
 
-const app = express();
+const app = express()
+app.use(express.static('public'));
 const port = env.port || 8080
-
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -16,12 +16,16 @@ app.get('/', async (req, res)=>{
     res.sendFile('index.html', { root: __dirname })
 })
 
+app.get('/grafico', async (req, res)=>{
+    res.sendFile('exemploGrafico.html', { root: __dirname })
+})
+
 app.get('/getTweets', async (req, res) => {
   try {
-    const tweets = await Tweets.findAll();
+    const tweets = await Tweets.findAll()
     res.json(tweets)
   } catch (error) {
-    console.error(error);
+    console.error(error)
     res.status(500).json({ message: 'Erro ao obter os tweets.' })
   }
 })
@@ -32,12 +36,12 @@ app.get('/getAuthors', async (req, res) => {
       attributes: [
         [Sequelize.fn('DISTINCT', Sequelize.col('author')), 'author']
       ]
-    });
+    })
 
-    res.json(authors);
+    res.json(authors)
   } catch (error) {
-    console.error('Erro ao selecionar autores distintos:', error);
-    res.status(500).json({ message: 'Erro ao selecionar autores distintos.' });
+    console.error('Erro ao selecionar autores distintos:', error)
+    res.status(500).json({ message: 'Erro ao selecionar autores distintos.' })
   }
 })
 
@@ -47,27 +51,46 @@ app.get('/getTags', async (req, res) => {
       attributes: [
         [Sequelize.fn('DISTINCT', Sequelize.col('lemmas')), 'tags']
       ]
-    });
+    })
 
-    res.json(tags);
+    res.json(tags)
   } catch (error) {
-    console.error('Error: ', error);
-    res.status(500).json({ message: 'Erro ao selecionar autores distintos.' });
+    console.error('Error: ', error)
+    res.status(500).json({ message: 'Erro ao selecionar autores distintos.' })
   }
 })
 
 app.get('/getTweetsByAuthors', async (req, res) => {
   try {
-    const authors = req.query.authors.split(',');
+    const authors = req.query.authors.split(',')
     const tweets = await   Tweets.findAll({
         where: {
             author: authors
         }
     })
-    res.json(tweets);
+    res.json(tweets)
   } catch (error) {
-    console.error('Error: ', error);
-    res.status(500).json({ message: 'Erro ao selecionar autores distintos.' });
+    console.error('Error: ', error)
+    res.status(500).json({ message: 'Erro ao selecionar autores distintos.' })
+  }
+})
+
+app.get('/getAuthorsByTag', async (req, res) => {
+  try {
+    const tag = req.query.tag
+    const authors = await Tweets.findAll({
+        where: {
+            lemmas: {
+                [Sequelize.Op.like]: `%${tag}%`
+            }
+        },
+        attributes: ['author'], 
+        group: ['author']
+    })
+    res.json(authors)
+  } catch (error) {
+    console.error('Error: ', error)
+    res.status(500).json({ message: 'Erro ao selecionar autores distintos.' })
   }
 })
 
